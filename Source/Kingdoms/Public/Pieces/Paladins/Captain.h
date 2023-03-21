6b7154e9d35a9d52c0f351c05b893ec44502265f
Sam Reitich <sam.reitich@gmail.com>
2023-03-21 10:54:18 -0700

    Created Captain class and mesh
    Created the Captain piece's class and completed its setup. Also created the Captain piece's mesh.

---
+++ Source/Kingdoms/Public/Pieces/Paladins/Captain.h
@@ -0,0 +1,17 @@
+// Copyright Change Studios, LLC 2023.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pieces/ParentPiece.h"
+#include "Captain.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KINGDOMS_API ACaptain : public AParentPiece
+{
+	GENERATED_BODY()
+	
+};
